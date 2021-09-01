c378fea7e30eb0067396856113ae3fb5ac9a7f9a
xMari75 <86401852+xMari75@users.noreply.github.com>
2021-09-01 10:57:24 +0200

    Add files via upload

--- filtry.php
+++ filtry.php
@@ -20,35 +20,39 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        //hodinky
         $vyrobci = array('Seiko', 'Certina', 'Luminox', 'Festina', 'Lotus');
         $modely = array('Model 1', 'Model 2', 'Model 3', 'Model 4', 'Model 5');
         $ceny = array(5700, 4500, 11970, 16800, 10900,);
         
+        //vlastnosti
         $zaruky = array('1 rok', '2 roky', '3 roky');
         $cifernik = array('Analogový', 'Digitální');
         $barva = array('Černá', 'Stříbrná', 'Modrá');
         
+        //kolekce hodinek
         $hodinky = array();
         for ($i = 0; $i < 100; $i++)
         {
             $hodinky[$i] = new Produkt($modely[rand(0, count($modely)-1)] . $i, $vyrobci[rand(0, count($vyrobci)-1)], $ceny[rand(0, count($ceny)-1)].' Kč');
             $hodinky[$i]->PridejVlastnost('Záruka', $zaruky[rand(0, count($zaruky)-1)]);
             $hodinky[$i]->PridejVlastnost('Ciferník',  $cifernik[rand(0, count($cifernik)-1)]);
-            if($i%2 == 0)
+            if($i%2 == 0) //ať nemají všechny vlastnosti
             {
                 $hodinky[$i]->PridejVlastnost('Barva',  $barva[rand(0, count($barva)-1)]);
             }
             
         }
         
+        //definice filtrů
         $filtrZaruka = new Vlastnost('Záruka', '2 roky');
         $filtrBarva = new Vlastnost('Barva', 'Modrá');
         $filtrCifernik = new Vlastnost('Ciferník', 'Analogový');
         
-        $poleFiltru = array($filtrZaruka, $filtrCifernik);//,$filtrBarva );
+        $poleFiltru = array($filtrZaruka, $filtrCifernik,$filtrBarva );
         
+        //výpis produktů
         foreach ($hodinky as $value) {
-            //echo "Y = $y <br />";
             $patri = false;
             for ($i = 0; $i < count($poleFiltru); $i++)
             {
@@ -57,7 +61,6 @@ and open the template in the editor.
                 {
                     break;
                 }
-                
             }
             if($patri)
             {
